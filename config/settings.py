aa0888ecff28c1630a0c3010205207b1ffef920b
evgeniy-kulikov <redstepway@gmail.com>
2022-08-27 20:57:26 +0300

    start lesson 04

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for production.
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -55,7 +55,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates", ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -63,13 +63,13 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "mainapp.context_processors.example.simple_context_processor",
             ],
         },
     },
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
-
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,6 @@
 # from django.http import HttpResponse
 # from django.shortcuts import render
-#
-# # Первоначальные страницы
-#
-# # Create your views here.
+
 # # Использование функции
 # # def hello_world(request):
 # #     return HttpResponse("Hello world")
@@ -15,11 +12,16 @@
 #     def get(self, *args):
 #         return HttpResponse("Hello world")
 #
-#
 # def check_kwargs(request, **kwargs):
 #     return HttpResponse(f"kwargs:<br>{kwargs}")
-
+import json
+import os
+from datetime import datetime
 from django.views.generic import TemplateView
+
+from config.settings import BASE_DIR
+
+file_ = open(os.path.join(BASE_DIR, 'mainapp/news.json'))
 
 
 class MainPageView(TemplateView):
@@ -28,6 +30,28 @@ class MainPageView(TemplateView):
 
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        # Get all previous data
+        context = super().get_context_data(**kwargs)
+        # Create your own data (собственные переменные проекта)
+        context["news_title"] = "Громкий новостной заголовок"
+        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        context["range"] = range(5)
+        context["datetime_obj"] = datetime.now()
+
+        context["params"] = ['django', 'python']
+        with file_:
+            news_data = json.load(file_)
+            context["news_data"] = news_data
+        return context
+
+
+class NewsWithPaginatorView(NewsPageView):
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
+        return context
 
 
 class CoursesPageView(TemplateView):
