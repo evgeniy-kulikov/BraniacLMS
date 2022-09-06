0f63b35a88986685ca5ef1921dee3501d3ea029b
evgeniy-kulikov <redstepway@gmail.com>
2022-09-06 20:42:45 +0300

    CRUD operations with News

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,31 +1,70 @@
-from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
+from django.shortcuts import get_object_or_404
+
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, TemplateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from mainapp import models as mainapp_models
+
+# from django.template.loader import render_to_string
+# from mainapp import forms as mainapp_forms
 
 
 class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
 
 
-class NewsPageView(TemplateView):
-    template_name = "mainapp/news.html"
+# class NewsPageView(TemplateView):
+#     template_name = "mainapp/news.html"
+#
+#     def get_context_data(self, **kwargs):
+#         # Get all previous data
+#         context = super().get_context_data(**kwargs)
+#         # Create your own data
+#         context["news_qs"] = mainapp_models.News.objects.all()[:5]
+#         return context
+#
+#
+# class NewsPageDetailView(TemplateView):
+#     template_name = "mainapp/news_detail.html"
+#
+#     def get_context_data(self, pk=None, **kwargs):
+#         context = super().get_context_data(pk=pk, **kwargs)
+#         context["news_object"] = get_object_or_404(mainapp_models.News, pk=pk)
+#         return context
 
-    def get_context_data(self, **kwargs):
-        # Get all previous data
-        context = super().get_context_data(**kwargs)
-        # Create your own data
-        context["news_qs"] = mainapp_models.News.objects.all()[:5]
-        return context
+
+class NewsListView(ListView):
+    model = mainapp_models.News
+    paginate_by = 5
+
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=False)
 
 
-class NewsPageDetailView(TemplateView):
-    template_name = "mainapp/news_detail.html"
+class NewsCreateView(PermissionRequiredMixin, CreateView):
+    model = mainapp_models.News
+    fields = "__all__"
+    success_url = reverse_lazy("mainapp:news")
+    permission_required = ("mainapp.add_news",)
 
-    def get_context_data(self, pk=None, **kwargs):
-        context = super().get_context_data(pk=pk, **kwargs)
-        context["news_object"] = get_object_or_404(mainapp_models.News, pk=pk)
-        return context
+
+class NewsDetailView(DetailView):
+    model = mainapp_models.News
+
+
+class NewsUpdateView(PermissionRequiredMixin, UpdateView):
+    model = mainapp_models.News
+    fields = "__all__"
+    success_url = reverse_lazy("mainapp:news")
+    permission_required = ("mainapp.change_news",)
+
+
+class NewsDeleteView(PermissionRequiredMixin, DeleteView):
+    model = mainapp_models.News
+    success_url = reverse_lazy("mainapp:news")
+    permission_required = ("mainapp.delete_news",)
 
 
 class CoursesListView(TemplateView):
@@ -46,6 +85,16 @@ class CoursesDetailView(TemplateView):
         context["lessons"] = mainapp_models.Lesson.objects.filter(course=context["course_object"])
         context["teachers"] = mainapp_models.CourseTeachers.objects.filter(course=context["course_object"])
         return context
+
+
+# class CourseFeedbackFormProcessView(LoginRequiredMixin, CreateView):
+#     model = mainapp_models.CourseFeedback
+#     form_class = mainapp_forms.CourseFeedbackForm
+#
+#     def form_valid(self, form):
+#         self.object = form.save()
+#         rendered_card = render_to_string("mainapp/includes/feedback_card.html", context={"item": self.object})
+#         return JsonResponse({"card": rendered_card})
 
 
 class ContactsPageView(TemplateView):
