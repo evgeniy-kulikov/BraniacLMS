fb43c0b49c3483da2a42be707e1767b29c105ffe
evgeniy-kulikov <redstepway@gmail.com>
2022-09-09 21:00:38 +0300

    Redis

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,13 @@ SECRET_KEY = "django-insecure-6%-==tg57)c2aqn)bxkq$@x7sx(vw!&$mz(_ko$yf#bs$i(0l@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+if DEBUG:
+    INTERNAL_IPS = [
+        "192.168.1.4",
+        "127.0.0.1",
+    ]
 
 # Application definition
 
@@ -40,6 +46,7 @@ INSTALLED_APPS = [
     "mainapp",
     "authapp",
     "crispy_forms",
+    "debug_toolbar",
     # "admin_interface",
 ]
 
@@ -51,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -197,4 +205,15 @@ LOGGING = {
             "handlers": ["file"],
         },
     },
+}
+
+# ребуется указать для Django тот backend, который используется в качестве кеш-системы
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
 }
